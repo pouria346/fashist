a383841b009326e64283185193eb4cddc6a3aaaa
pouria3463 <08310831p@gmail.com>
2016-02-25 14:34:03 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {102611386,tonumber(205152033)},--Sudo users
+    sudo_users = {152831755,102611386,tonumber(205152033)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
